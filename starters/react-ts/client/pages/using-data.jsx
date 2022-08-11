521a9fd225be6df83f5f51feffa6688dcf7e73a3
Gustavo <gustavo.aguiar@redspark.io>
2022-08-11 11:53:23 -0300

    a

--- starters/react-ts/client/pages/using-data.jsx
+++ starters/react-ts/client/pages/using-data.jsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useRouteContext } from "/dx:core.jsx";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useRouteContext } from '/dx:core.jsx'
 
-export function getMeta() {
-  return { title: "Todo List — Using Data" };
+export function getMeta () {
+  return { title: 'Todo List — Using Data' }
 }
 
-export function getData({ server }) {
-  return {
-    todoList: server.db.todoList,
-  };
+export function getData ({ server }) {
+	return {
+		todoList: server.db.todoList
+	}
 }
 
-export default function Index(props) {
-  const { data } = useRouteContext();
-  const [todoList, updateTodoList] = useState(data.todoList);
-  const [input, setInput] = useState(null);
+export default function Index (props) {
+  const {data} = useRouteContext()
+  const [todoList, updateTodoList] = useState(data.todoList)
+  const [input, setInput] = useState(null)
   const addItem = (value) => {
-    updateTodoList((list) => [...list, value]);
-    input.value = "";
-  };
+    updateTodoList(list => [...list, value])
+    input.value = ''
+  }
   return (
     <>
       <h2>Todo List — Using Data</h2>
-      <ul>
-        {todoList.map((item, i) => {
-          return <li key={`item-${i}`}>{item}</li>;
-        })}
-      </ul>
+      <ul>{
+        todoList.map((item, i) => {
+          return <li key={`item-${i}`}>{item}</li>
+        })
+      }</ul>
       <div>
         <input ref={setInput} />
         <button onClick={() => addItem(input.value)}>Add</button>
@@ -36,14 +36,11 @@ export default function Index(props) {
         <Link to="/">Go back to the index</Link>
       </p>
       <p>⁂</p>
-      <p>
-        When you navigate away from this route, any additions to the to-do list will
-        be lost, because they're bound to this route component only.
-      </p>
-      <p>
-        See the <Link to="/using-store">/using-store</Link> example to learn how to
-        use the application global state for it.
+      <p>When you navigate away from this route, any additions to the to-do 
+      list will be lost, because they're bound to this route component only.</p>
+      <p>See the <Link to="/using-store">/using-store</Link> example to learn 
+      how to use the application global state for it.
       </p>
     </>
-  );
+  )
 }
--- starters/react-ts/client/pages/using-store.jsx
+++ starters/react-ts/client/pages/using-store.jsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useRouteContext } from "/dx:core.jsx";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useRouteContext } from '/dx:core.jsx'
 
-export function getMeta() {
-  return { title: "Todo List — Using Store" };
+export function getMeta () {
+  return { title: 'Todo List — Using Store' }
 }
 
-export default function Index(props) {
-  const { snapshot, state, actions } = useRouteContext();
-  const [input, setInput] = useState(null);
+export default function Index (props) {
+  const {snapshot, state, actions} = useRouteContext()
+  const [input, setInput] = useState(null)
   const addItem = async (value) => {
-    await actions.addTodoItem(state, value);
-    input.value = "";
-  };
+    await actions.addTodoItem(state, value)
+    input.value = ''
+  }
   return (
     <>
       <h2>Todo List — Using Store</h2>
-      <ul>
-        {snapshot.todoList.map((item, i) => {
-          return <li key={`item-${i}`}>{item}</li>;
-        })}
-      </ul>
+      <ul>{
+        snapshot.todoList.map((item, i) => {
+          return <li key={`item-${i}`}>{item}</li>
+        })
+      }</ul>
       <div>
         <input ref={setInput} />
         <button onClick={() => addItem(input.value)}>Add</button>
@@ -29,10 +29,8 @@ export default function Index(props) {
         <Link to="/">Go back to the index</Link>
       </p>
       <p>⁂</p>
-      <p>
-        When you navigate away from this route, any additions to the to-do list are
-        not lost, because they're bound to the global application state.
-      </p>
+      <p>When you navigate away from this route, any additions to the to-do 
+      list are not lost, because they're bound to the global application state.</p>
     </>
-  );
+  )
 }
